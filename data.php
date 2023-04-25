4612960de3f8ad94f1a64fa6f61cd0fdec1d5d74
Erbolat Bolatbekuly Tursynbek <erbolat_tursynbek@hotmail.com>
2023-04-25 11:42:51 +0600

    init:11:42

--- data.php
+++ data.php
@@ -5,7 +5,7 @@
 	define('db_host','localhost');
 	define('db_user','root');
 	define('db_pass','');
-	define('db_name','indikativ_2023');
+	define('db_name','indikativ_2022');
 
 	
 	$mysqli = new mysqli(db_host,db_user,db_pass,db_name);
